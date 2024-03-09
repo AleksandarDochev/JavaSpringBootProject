f2a187943194efaa00211297250ac9e0454769e0
Alex <aleksandaredochev@gmail.com>
2024-03-10 00:24:43 +0200

    Bean Life Cycle

--- src/main/java/com/SpringApp/springboot/demo/SpringBootProject/common/CricketCoach.java
+++ src/main/java/com/SpringApp/springboot/demo/SpringBootProject/common/CricketCoach.java
@@ -1,5 +1,7 @@
 package com.SpringApp.springboot.demo.SpringBootProject.common;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Scope;
@@ -13,8 +15,19 @@ public class CricketCoach implements Coach{
     public CricketCoach(){
         System.out.println("In constructor: " +getClass().getSimpleName());
     }
+    //define initial method
+    @PostConstruct
+    public void doMyStartupStuff(){
+        System.out.println("In doMyStartupStuff(): " + getClass().getSimpleName());
+
+    }    //define our destroy method
+    @PreDestroy
+    public void doMyCleanupStuff(){
+        System.out.println("In ddoMyCleanupStuff(): " + getClass().getSimpleName());
+    }
     @Override
     public String getDailyWorkout() {
         return "Practice fast bowling for 15 minutes";
     }
+
 }
