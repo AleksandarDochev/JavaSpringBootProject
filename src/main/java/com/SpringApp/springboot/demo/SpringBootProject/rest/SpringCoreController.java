4716aec71043837aa57f6c826528b02f509d9498
Alex <aleksandaredochev@gmail.com>
2024-03-09 23:04:19 +0200

    Qualifiers and multiple beans for same interface

--- src/main/java/com/SpringApp/springboot/demo/SpringBootProject/rest/SpringCoreController.java
+++ src/main/java/com/SpringApp/springboot/demo/SpringBootProject/rest/SpringCoreController.java
@@ -3,6 +3,7 @@ package com.SpringApp.springboot.demo.SpringBootProject.rest;
 import com.SpringApp.springboot.demo.SpringBootProject.common.Coach;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,15 +14,16 @@ public class SpringCoreController {
     //define private field for the dependency
     private Coach myCoach;
     //Define constructor for dependency injection
-//    @Autowired
-//    public SpringCoreController(Coach theCoach){
-//        myCoach=theCoach;
-//    }
-    //Define setter for dependency injection
+    //and the @Qualifier is given starting with a small letter ,
     @Autowired
-    public void setCoach(Coach theCoach){
+    public SpringCoreController(@Qualifier("trackCoach") Coach theCoach){
         myCoach=theCoach;
     }
+    //Define setter for dependency injection
+//    @Autowired
+//    public void setCoach(Coach theCoach){
+//        myCoach=theCoach;
+//    }
 
     @GetMapping("/dailyworkout")
     public String getDailyWorkout(){
