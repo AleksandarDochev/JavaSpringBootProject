be0ecb490f6b2b1d875b1bab634856b1de3bb2c5
Alex <aleksandaredochev@gmail.com>
2024-07-19 23:33:04 +0300

    MASSIVE REFACTOR adding RestApiCrudWebServicesSpring

---
+++ RestApiCrudWebServicesSpring/src/test/java/com/RestAPI/crud/RestApiCrudWebServicesSpring/RestApiCrudWebServicesSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RestAPI.crud.RestApiCrudWebServicesSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiCrudWebServicesSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
