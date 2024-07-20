cdc2bcbe375c5e038e37155a5fbf8cf4fb941048
Alex <aleksandaredochev@gmail.com>
2024-07-20 21:08:06 +0300

    REST APIs Web Services Spring REST concepts JSON HTTP messaging REST client tool Postman ,@RestController -data loader for the REST controller

--- Module_RESTapiWebServicesJsonHTTPmessagingPostaman/src/main/java/com/TheDevCoffeePoint/RESTapiWebServicesJsonHTTPmessagingPostaman/rest/StudentRestController.java
+++ Module_RESTapiWebServicesJsonHTTPmessagingPostaman/src/main/java/com/TheDevCoffeePoint/RESTapiWebServicesJsonHTTPmessagingPostaman/rest/StudentRestController.java
@@ -1,6 +1,7 @@
 package com.TheDevCoffeePoint.RESTapiWebServicesJsonHTTPmessagingPostaman.rest;
 
 import com.TheDevCoffeePoint.RESTapiWebServicesJsonHTTPmessagingPostaman.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,13 +12,23 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
-    //define endpoint for "/students" - return list of students
-    @GetMapping("/students")
-    public List<Student> getStudents(){
-        List<Student> theStudents = new ArrayList<Student>();
+    //define a field , load the fields so data entry is done separately each and every time
+    private List<Student> theStudents;
+    //define @PostConstruct to load the student data ... only once
+    @PostConstruct
+    public void loadData (){
+
+        theStudents = new ArrayList<>();
+
         theStudents.add(new Student("Poornima", "Patel"));
         theStudents.add(new Student("Mario", "Rossi"));
         theStudents.add(new Student("Mary", "Smith"));
+    }
+    //define endpoint for "/students" - return list of students
+    @GetMapping("/students")
+    public List<Student> getStudents(){
+        //access for Students REST service API http://localhost:7070/api/students
+
         return theStudents;
     }
 }
