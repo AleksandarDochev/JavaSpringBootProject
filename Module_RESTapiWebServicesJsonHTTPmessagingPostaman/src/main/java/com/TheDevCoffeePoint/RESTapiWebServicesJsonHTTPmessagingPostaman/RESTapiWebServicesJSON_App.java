f178a0217411796f52090a0b849218df3fb20e98
Alex <aleksandaredochev@gmail.com>
2024-07-20 17:41:12 +0300

    adding other module

---
+++ Module_RESTapiWebServicesJsonHTTPmessagingPostaman/src/main/java/com/TheDevCoffeePoint/RESTapiWebServicesJsonHTTPmessagingPostaman/RESTapiWebServicesJSON_App.java
@@ -0,0 +1,14 @@
+package com.TheDevCoffeePoint.RESTapiWebServicesJsonHTTPmessagingPostaman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RESTapiWebServicesJSON_App {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RESTapiWebServicesJSON_App.class, args);
+		//access http://localhost:7070/test/hello
+	}
+
+}
---
+++ Module_RESTapiWebServicesJsonHTTPmessagingPostaman/src/test/java/com/TheDevCoffeePoint/RESTapiWebServicesJsonHTTPmessagingPostaman/RESTapiWebServicesJSONAppTests.java
@@ -0,0 +1,13 @@
+package com.TheDevCoffeePoint.RESTapiWebServicesJsonHTTPmessagingPostaman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RESTapiWebServicesJSONAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
