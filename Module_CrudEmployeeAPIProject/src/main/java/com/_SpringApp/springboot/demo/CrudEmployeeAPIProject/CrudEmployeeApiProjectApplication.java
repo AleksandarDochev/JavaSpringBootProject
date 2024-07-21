2ea1958bf20bc3cec83e7b3207525b52c5d2b29f
Alex <aleksandaredochev@gmail.com>
2024-07-21 16:51:36 +0300

    New Maven Module creating Guide + new module CrudEmployeeAPIProject

---
+++ Module_CrudEmployeeAPIProject/src/main/java/com/_SpringApp/springboot/demo/CrudEmployeeAPIProject/CrudEmployeeApiProjectApplication.java
@@ -0,0 +1,13 @@
+package com._SpringApp.springboot.demo.CrudEmployeeAPIProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudEmployeeApiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudEmployeeApiProjectApplication.class, args);
+	}
+
+}
