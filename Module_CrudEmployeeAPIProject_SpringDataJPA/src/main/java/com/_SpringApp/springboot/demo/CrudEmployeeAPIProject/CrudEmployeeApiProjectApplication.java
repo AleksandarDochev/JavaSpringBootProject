08743ecc495001879bb5c0cffb682c9cac46a877
Alex <aleksandaredochev@gmail.com>
2024-07-24 08:32:30 +0300

    REST APIs Web Services Spring REST concepts JSON HTTP messaging REST client tool Postman ,@RestController -Spring Data JPA

---
+++ Module_CrudEmployeeAPIProject_SpringDataJPA/src/main/java/com/_SpringApp/springboot/demo/CrudEmployeeAPIProject/CrudEmployeeApiProjectApplication.java
@@ -0,0 +1,29 @@
+package com._SpringApp.springboot.demo.CrudEmployeeAPIProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudEmployeeApiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudEmployeeApiProjectApplication.class, args);
+		//test http://localhost:6060/api/employees
+		//test by ID http://localhost:6060/api/employees/2
+
+		//test POST  in JSON format -> JSON_howToTestEndpointsGuide
+		//Endpoint test to POST in JSON format
+		//POST http://localhost:6060/api/employees
+		//the requested body is provided in place
+		//Content-Type: application/json
+		//
+		////
+		//{
+        //  "name": "John Doe",
+        //  "position": "Software Engineer",
+        //  "department": "IT",
+        //  "salary": 80000
+        //}
+	}
+
+}
---
+++ Module_CrudEmployeeAPIProject_SpringDataJPA/src/test/java/com/_SpringApp/springboot/demo/CrudEmployeeAPIProject/CrudEmployeeApiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com._SpringApp.springboot.demo.CrudEmployeeAPIProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudEmployeeApiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
