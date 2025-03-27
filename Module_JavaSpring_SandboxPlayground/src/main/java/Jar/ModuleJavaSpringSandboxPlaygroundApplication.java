a350e87b62e8e6b363135f52f31f1c2309be26f8
Alex <aleksandaredochev@gmail.com>
2025-03-27 17:50:28 +0200

    added Module_JavaSpring_SandboxPlayground

---
+++ Module_JavaSpring_SandboxPlayground/src/main/java/Jar/ModuleJavaSpringSandboxPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package Jar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuleJavaSpringSandboxPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModuleJavaSpringSandboxPlaygroundApplication.class, args);
+	}
+
+}
---
+++ Module_JavaSpring_SandboxPlayground/src/test/java/Jar/ModuleJavaSpringSandboxPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package Jar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuleJavaSpringSandboxPlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
