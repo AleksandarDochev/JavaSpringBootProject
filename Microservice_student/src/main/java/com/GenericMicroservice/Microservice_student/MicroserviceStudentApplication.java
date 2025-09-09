51e1051c45167b12d8bef0efdcb9398c0f6ae665
Alex <aleksandaredochev@gmail.com>
2025-09-09 23:47:24 +0300

    Microservice Student - Student Module implementation and configuration

---
+++ Microservice_student/src/main/java/com/GenericMicroservice/Microservice_student/MicroserviceStudentApplication.java
@@ -0,0 +1,13 @@
+package com.GenericMicroservice.Microservice_student;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceStudentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceStudentApplication.class, args);
+	}
+
+}
