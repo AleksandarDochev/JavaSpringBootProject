c88ae4d8f3e2fbe7e55f477c444eeda9d7953cb6
Alex <aleksandaredochev@gmail.com>
2025-09-12 00:42:09 +0300

    Microservice Student  - Student controller

---
+++ Microservice_student/src/main/java/com/GenericMicroservice/Microservice_student/entity/StudentController.java
@@ -0,0 +1,25 @@
+package com.GenericMicroservice.Microservice_student.entity;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/students")
+@RequiredArgsConstructor
+public class StudentController {
+    private final StudentService service;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void save(@RequestBody Student student){
+        service.saveStudent(student);
+    }
+    @GetMapping
+    public ResponseEntity<List<Student>> findAllStudents(){
+        return ResponseEntity.ok(service.findAllStudents());
+    }
+}
